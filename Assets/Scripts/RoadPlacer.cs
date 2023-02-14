d66ec9ddc1fceb7d25969190ebffbe22a7cbbf5d
unknown <ivantorresoses2@gmail.com>
2023-02-14 20:03:07 +0100

    updated version

--- Assets/Scripts/RoadPlacer.cs
+++ Assets/Scripts/RoadPlacer.cs
@@ -7,29 +7,83 @@ using UnityEngine;
 
 public class RoadPlacer : MonoBehaviour
 {
+    [Header("General")]
     [SerializeField] private GameObject road;
+    [Tooltip("This will be the layer the roads are placed in")]
     [SerializeField] private LayerMask roadLayer;
     [Tooltip("Load the textures in this order: \n0 - Solo Road\n1 - Deadend road\n2 - Straight Road\n3 - Three way Road\n4 - Four way road\n5 - Corner road\n\nMake sure that they have the same orientation as the ones provided")]
     [SerializeField] private List<Texture> roadTextures = new List<Texture>();
+
+
+    [Header("Options")]
+    [Tooltip("Uncheck this if you don't want to use the WaypointManager system")]
+    [SerializeField] private bool useWaypoints = true;
+    [Tooltip("How often does it try to place a road when holding down a button, the lower the number the more times it will check for it.\n A really low number may cause lag")]
+    [Range(0.0001f, 1f)]
+    [SerializeField] private float tryPlaceRoadCd = 0.05f;
+
 
     private bool isPlacing = false;
     private WaypointManager waypointManager;
 
     private void Start()
     {
-        waypointManager = GetComponent<WaypointManager>();
+        if(roadTextures.Count != 6)
+        {
+            Debug.LogError("There are not enough textures in the road textures field! Make sure to add them properly");
+        }
+
+        if(useWaypoints)
+            waypointManager = GetComponent<WaypointManager>();
         StartCoroutine(PlaceRoad());
     }
 
     private void Update()
     {
         if(Input.GetMouseButtonDown(0))
-           isPlacing = true;
-        else if (Input.GetMouseButtonUp(0))
-            isPlacing = false;
+        {
+            PlaceSingleRoad();
+        }
 
-        if (Input.GetMouseButtonDown(1))
-            StartCoroutine(TryRemoveRoad());
+        if(Input.GetMouseButton(0))
+        {
+            MultipleRoads(true);
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            MultipleRoads(false);
+        }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            RemoveRoad();
+        }
+    }
+
+    /// <summary>
+    /// Call this function with the Input.GetDown function
+    /// </summary>
+    public void PlaceSingleRoad()
+    {
+        TryPlaceRoad();
+    }
+
+    /// <summary>
+    /// Call this function with the Input.Get() function
+    /// </summary>
+    /// <param name="_isPlacing"></param>
+    public void MultipleRoads(bool _isPlacing)
+    {
+        this.isPlacing = _isPlacing;
+    }
+
+    /// <summary>
+    /// Call this function when you want to delete a road and have its neighbours update
+    /// </summary>
+    public void RemoveRoad()
+    {
+        StartCoroutine(TryRemoveRoad());
     }
 
     private IEnumerator TryRemoveRoad()
@@ -40,10 +94,9 @@ public class RoadPlacer : MonoBehaviour
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, roadLayer))
         {
             pos = hit.collider.gameObject.transform.position;
-            waypointManager.RemoveWaypoint(hit.collider.gameObject.transform.position);
-            Destroy(hit.collider.gameObject);
-            
-            
+            if (useWaypoints)
+                waypointManager.RemoveWaypoint(hit.collider.gameObject.transform.position);
+            Destroy(hit.collider.gameObject);      
         }
         yield return new WaitForSeconds(.01f);
         if(pos.y != -100)
@@ -80,7 +133,7 @@ public class RoadPlacer : MonoBehaviour
     {
         while(true)
         {
-            yield return new WaitForSeconds(.05f);
+            yield return new WaitForSeconds(tryPlaceRoadCd);
             if(isPlacing)
             {
                 TryPlaceRoad();
@@ -151,24 +204,27 @@ public class RoadPlacer : MonoBehaviour
             }
         }
 
-        Debug.Log(neighbours.Count);
         SetTextureAndRotation(roadAux, directions, neighbours.Count);
        
     }
 
     private void SetTextureAndRotation(GameObject roadAux, List<bool> directions, int neighbours)
     {
-        Debug.Log(directions[0]);
         roadAux.transform.rotation = Quaternion.identity;
+        int textureIndex = 0;
 
         if (neighbours == 4)
         {
-            waypointManager.AddWaypoint(roadAux.transform.position);
-            roadAux.GetComponent<Renderer>().material.SetTexture("_MainTex", roadTextures[4]);
+            if (useWaypoints)
+                waypointManager.AddWaypoint(roadAux.transform.position);
+            textureIndex = 4;
         } else if(neighbours == 3)
         {
-            waypointManager.AddWaypoint(roadAux.transform.position);
-            roadAux.GetComponent<Renderer>().material.SetTexture("_MainTex", roadTextures[3]);
+            if (useWaypoints)
+                waypointManager.AddWaypoint(roadAux.transform.position);
+
+            textureIndex = 3;
+
             if (directions[3] && !directions[2]) // There is one below not one on top
                 roadAux.transform.rotation = Quaternion.Euler(new Vector3(0f, 180f, 0f));
             else if(!directions[3] && directions[2]) // There is one on top not one below
@@ -177,23 +233,32 @@ public class RoadPlacer : MonoBehaviour
                 roadAux.transform.rotation = Quaternion.Euler(new Vector3(0f, 90f, 0f));
             else // Top and Below and left
                 roadAux.transform.rotation = Quaternion.Euler(new Vector3(0f, -90f, 0f));
+
         } else if(neighbours == 2)
         {
             if(directions[0] && directions[1]) // horizontal
             {
-                waypointManager.RemoveWaypoint(roadAux.transform.position);
-                roadAux.GetComponent<Renderer>().material.SetTexture("_MainTex", roadTextures[2]);
+                if (useWaypoints)
+                    waypointManager.RemoveWaypoint(roadAux.transform.position);
+
+                textureIndex = 2;
+
                 roadAux.transform.rotation = Quaternion.Euler(new Vector3(0f, 90f, 0f));
 
             } else if(directions[2] && directions[3]) // Vertical
             {
-                waypointManager.RemoveWaypoint(roadAux.transform.position);
-                roadAux.GetComponent<Renderer>().material.SetTexture("_MainTex", roadTextures[2]);
+                if (useWaypoints)
+                    waypointManager.RemoveWaypoint(roadAux.transform.position);
+
+                textureIndex = 2;
             }
             else // We have a turn
             {
-                waypointManager.AddWaypoint(roadAux.transform.position);
-                roadAux.GetComponent<Renderer>().material.SetTexture("_MainTex", roadTextures[5]);
+                if (useWaypoints)
+                    waypointManager.AddWaypoint(roadAux.transform.position);
+
+                textureIndex = 5;
+
                 if (directions[0]) // Right
                 {
                     if (directions[2]) // Top
@@ -211,8 +276,11 @@ public class RoadPlacer : MonoBehaviour
             }
         } else if (neighbours == 1)
         {
-            waypointManager.AddWaypoint(roadAux.transform.position);
-            roadAux.GetComponent<Renderer>().material.SetTexture("_MainTex", roadTextures[1]);
+            if (useWaypoints)
+                waypointManager.AddWaypoint(roadAux.transform.position);
+
+            textureIndex = 1;
+
             if (directions[1])
                 roadAux.transform.rotation = Quaternion.Euler(new Vector3(0f, -90f, 0f));
             else if(directions[0])
@@ -223,8 +291,25 @@ public class RoadPlacer : MonoBehaviour
             }
         } else
         {
-            waypointManager.RemoveWaypoint(roadAux.transform.position);
-            roadAux.GetComponent<Renderer>().material.SetTexture("_MainTex", roadTextures[0]);
+            if (useWaypoints)
+                waypointManager.RemoveWaypoint(roadAux.transform.position);
+
+            textureIndex = 0;
         }
+
+        ChangeTexture(roadAux, roadTextures[textureIndex]);
     } 
+
+    private void ChangeTexture(GameObject road, Texture newTexture)
+    {
+        Renderer renderer;
+
+        if(road.TryGetComponent<Renderer>(out renderer))
+        {
+            renderer.material.mainTexture = newTexture;
+        } else
+        {
+            road.GetComponentInChildren<Renderer>().material.mainTexture = newTexture;
+        }
+    }
 }
